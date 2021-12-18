41c4abba2e90448fa6c23918920d76e0d35f2b18
Thomas simons <thomassimons98@gmail.com>
2021-12-18 14:51:27 +0000

    Add project files.

---
+++ ContosoCraft.WebSite/Controller/ProductsController.cs
@@ -0,0 +1,42 @@
+﻿using ContosoCraft.WebSite.Models;
+using ContosoCraft.WebSite.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContosoCraft.WebSite.Controller
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+
+        public ProductsController(JsonFileProductService productService)
+        {
+            this.ProductService = productService;
+        }
+
+        public JsonFileProductService ProductService { get; }
+        
+        [HttpGet]
+        public IEnumerable<Product> Get()
+        {
+            return ProductService.GetProducts();
+        }
+
+        //use this link to call the api. this will add a rating of 5 to jenlooper-cactus.
+        //https://localhost:44345/products/rate?ProductId=jenlooper-cactus&rating=5
+        [Route("rate")]
+        [HttpGet]
+        public ActionResult Get(
+            [FromQuery] string ProductId, 
+            [FromQuery] int Rating)
+        {
+            ProductService.AddRating(ProductId, Rating);
+            return Ok();
+        }
+    }
+}
